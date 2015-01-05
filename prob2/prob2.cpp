6fa32414b4cfd33950868440ad554cf384c70a24
tanleach <tanleach1001@gmail.com>
2015-01-05 17:33:57 -0500

    prjectEuler.net Problem 2: By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
    Using C++.  Not OOP because of the simplicity. Use as you wish.

---
+++ prob2/prob2.cpp
@@ -0,0 +1,46 @@
+/*  
+ *  Tanner Leach
+ *  November 7, 2013
+ *  sum of all even terms in a fibonacii sequence whose value does 
+ *  not exceed four million.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int);
+
+int main(){
+    
+    //preloading the first two fib numbers and sums
+    long int fib1 = 1;
+    long int fib2 = 1;
+    long int fibSum = 2;
+    
+    //loading the sum of the first even number fib.
+    int evenSum = 2;
+
+    while(fibSum <= 4000000){
+        fib1 = fib2;
+        fib2 = fibSum;
+        fibSum = fib1 + fib2;
+
+        //Only add sums if the fib value is even.
+        if((fibSum % 2) == 0){
+            evenSum += fibSum;
+        }
+    }
+
+    cout << "Sum: " << evenSum << endl;
+
+    return 0;
+
+}
+
+int fibonacci(int idx){
+    if (idx == 0) return 0;
+    if (idx == 1) return 1;
+
+    return (fibonacci(idx - 1) + fibonacci(idx - 2));
+}
---
+++ prob2/prob2.java
@@ -0,0 +1,23 @@
+/*
+ * @author Tanner Leach
+ *
+ * Sum of all even-valued terms in a fibonacci sequence 
+ * whose value does not exceed four million.
+ */
+
+public class prob2{
+
+    //preloading the fib sequence
+    public int fib1 = 1;
+    public int fib2 = 1;
+    public int fibSum = 2;
+
+    //Loading the sum of the first even number fib.
+    public int evenSum = 2;
+
+    public String toString(){
+        return "Fib. sum: " + getSum();
+    }
+
+    public int getNextFib()
+}
