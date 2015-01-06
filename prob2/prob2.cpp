9aa67e94e19d2f421507c533db959f22fb24bddb
tanleach <tanleach1001@gmail.com>
2015-01-05 20:11:56 -0500

    Finished the Java version for Problem 2 of projecteuler.net.

--- prob2/prob2.cpp
+++ prob2/prob2.cpp
@@ -38,9 +38,3 @@ int main(){
 
 }
 
-int fibonacci(int idx){
-    if (idx == 0) return 0;
-    if (idx == 1) return 1;
-
-    return (fibonacci(idx - 1) + fibonacci(idx - 2));
-}
--- prob2/prob2.java
+++ prob2/prob2.java
@@ -8,16 +8,46 @@
 public class prob2{
 
     //preloading the fib sequence
-    public int fib1 = 1;
-    public int fib2 = 1;
-    public int fibSum = 2;
+    public long fib1 = 1;
+    public long fib2 = 1;
+    public long fibSum = 2;
 
     //Loading the sum of the first even number fib.
-    public int evenSum = 2;
+    public long evenSum = 2;
 
     public String toString(){
-        return "Fib. sum: " + getSum();
+        return "Fib. sum: " + evenSum;
     }
 
-    public int getNextFib()
+    public void getNextFib(){
+        fib1    = fib2;
+        fib2    = fibSum;
+        fibSum  = fib1 + fib2;
+    }
+
+    /*
+     * Keeps running sum of fib. numbers that are even.
+     */
+    public void addIfEven(){
+        if((fibSum % 2) == 0)
+            evenSum += fibSum;
+    }
+
+    /*
+     * A getter for the fubSum()
+     * @return <code>Long</code> fidSum
+     */
+    public long getSum(){
+        return fibSum;
+    }
+
+    public static void main(String[] args){
+        prob2 fibonacci = new prob2();
+        
+        while(fibonacci.getSum() <= 4000000){
+            fibonacci.getNextFib();
+            fibonacci.addIfEven();
+        }
+        System.out.println(fibonacci);
+    }
 }
